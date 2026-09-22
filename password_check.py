99b3040173ca24f28851a287f95b7e8bdac65cc2
AnastasiK8 <anastasiyakostiuchenko15@gmail.com>
2026-09-22 15:02:00 +0300

    Навмисна помилка для демонстрації СІ

--- password_check.py
+++ password_check.py
@@ -37,7 +37,7 @@ def is_strong_password(p):
     has_upper = any(ch.isupper() for ch in p)
     has_lower = any(ch.islower() for ch in p)
 
-    return has_digit and has_upper and has_lower
+    return has_digit or has_upper or has_lower
 
 
 def count_digits(p):
